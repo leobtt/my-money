0c34d8590eb1fb90cc9954f7247b5015deced27c
leobtt <leonarbat8@gmail.com>
2021-12-26 20:19:34 -0300

    Home

---
+++ src/page/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddMonth from './AddMonth'
+import Months from './Moths'
+
+const Home = () => {
+  return (
+    <div className="container-md py-3">
+      <AddMonth />
+      <Months />
+    </div>
+  )
+}
+
+export default Home
