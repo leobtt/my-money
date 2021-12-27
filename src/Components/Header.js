24ff7f360554f4608d093ea7dc64911f282ac9d9
leobtt <leonarbat8@gmail.com>
2021-12-27 00:28:26 -0300

    link to home

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
       <nav className="nav bg-dark text-white py-3 mb-4">
         <div className="container-md">
-          <h1>My Money</h1>
+          <h1>
+            <Link className="text-white text-decoration-none" to="/">
+              My Money
+            </Link>
+          </h1>
         </div>
       </nav>
       <div className="container-md">
