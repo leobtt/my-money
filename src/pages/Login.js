8bdc00fde35cb21e70bfc874a0d794c701d32391
leobtt <leonarbat8@gmail.com>
2022-01-29 00:09:19 -0300

    log in

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,30 +1,62 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Navigate } from 'react-router-dom'
 import usePost from '../hooks/usePost'
 
 const url =
   'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAHvc0ac3U9O9A0iqSn49UjMN2i4S0A16g'
 
 const Login = () => {
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  })
   const [postData, postLogin] = usePost(url)
 
   useEffect(() => {
     if (Object.keys(postData.data).length > 0) {
       localStorage.setItem('token', postData.data.idToken)
-      console.log('logou')
     }
   }, [postData])
 
-  const login = async () => {
+  if (postData.data.idToken) {
+    return <Navigate replace to="/" />
+  }
+
+  const handleChange = (evt) => {
+    setLogin({
+      ...login,
+      [evt.target.name]: evt.target.value,
+    })
+  }
+
+  const logIn = async () => {
     await postLogin({
-      email: 'leonarbat8@gmail.com',
-      password: '',
+      email: login.email,
+      password: login.password,
       returnSecureToken: true,
     })
   }
   return (
     <>
-      <button onClick={login}>Login page</button>
-      <p>{JSON.stringify(postData, null, 4)}</p>
+      <input
+        type="text"
+        placeholder="e-mail"
+        name="email"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        name="password"
+        onChange={handleChange}
+      />
+      <button type="button" onClick={logIn}>
+        Login page
+      </button>
+      {postData.error === 'INVALID_PASSWORD' && <p>usuário inválido</p>}
+      <p>{JSON.stringify(login, null, 4)}</p>
+      <br></br>
+      <p>{JSON.stringify(postData.error, null, 4)}</p>
     </>
   )
 }
