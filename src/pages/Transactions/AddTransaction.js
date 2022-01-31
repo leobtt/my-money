c8a3f6d31911380f696aa436b23bbe34e3d7ddfa
leobtt <leonarbat8@gmail.com>
2022-01-31 00:01:30 -0300

    post

--- src/pages/Transactions/AddTransaction.js
+++ src/pages/Transactions/AddTransaction.js
@@ -1,21 +1,26 @@
 import { useState } from 'react'
 
-const AddTransaction = ({ saveTransaction, transaction }) => {
+const month = () => {
+  const result = new Date().getMonth() + 1
+  return '0' + result
+}
+
+const AddTransaction = ({ saveTransaction, transaction, infoMonth }) => {
   const [form, setForm] = useState({
     descricao: '',
     valor: 0,
+    categoria: 'Salário',
   })
   const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time))
-
   const getValue =
     (field) =>
     ({ target }) => {
       setForm({
         ...form,
         [field]:
-          field === 'descricao'
-            ? target.value
-            : parseFloat(target.value.replace(/,/g, '.')).toFixed(2),
+          field === 'valor'
+            ? parseFloat(target.value.replace(/,/g, '.')).toFixed(2)
+            : target.value,
       })
     }
 
@@ -29,22 +34,56 @@ const AddTransaction = ({ saveTransaction, transaction }) => {
       await saveTransaction({
         ...form,
         valor: parseFloat(form.valor),
+        receita: form.categoria === 'Salário' ? true : false,
+        data: `${new Date().getDate()}/${month()}/${new Date().getFullYear()}`,
       })
-      transaction.refetch()
-      await sleep(5000)
-      //infoMonth.refetch()
+      sleep(5000)
+      await transaction.refetch()
+      await infoMonth.refetch()
     }
   }
 
   return (
     <>
-      <tr>
+      {JSON.stringify(form, null, 2)}
+      <div className="flex-transaction">
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <input
+            onChange={getValue('valor')}
+            value={form.value}
+            type="text"
+            placeholder="valor"
+          />
+
+          <input
+            onChange={getValue('descricao')}
+            value={form.description}
+            type="text"
+            placeholder="descrição"
+          />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <select name="categoria" onChange={getValue('categoria')}>
+            <option value="Salário">Salário</option>
+            <option value="Compras">Compras</option>
+            <option value="Alimentação">Alimentação</option>
+            <option value="Saúde">Saúde</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Estudos">Estudos</option>
+            <option value="Locomoção">Locomoção</option>
+          </select>
+        </div>
+        <button type="button" onClick={save}>
+          Adicionar
+        </button>
+      </div>
+      {/* <tr>
         <td>
           <input
             onChange={getValue('descricao')}
             value={form.description}
             type="text"
-          ></input>
+          />
         </td>
         <td>
           <input
@@ -58,7 +97,7 @@ const AddTransaction = ({ saveTransaction, transaction }) => {
             +
           </button>
         </td>
-      </tr>
+      </tr> */}
     </>
   )
 }
